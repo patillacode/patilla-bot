68c0c75309e48b416eb4e6218b72e07054d7a60a
patillacode <patillacode@gmail.com>
2016-02-10 19:12:28 +0000

    Added new kitten pics. Edited names in old pics.

--- kittens.js
+++ kittens.js
@@ -2,8 +2,9 @@ fs = require('fs');
 config = require('./config');
 
 function getKittenImagePath(){
-    file_list = fs.readdirSync(config.images_path);
-    return config.images_path+"/kitten_"+Math.floor(Math.random() * file_list.length)+".jpg";
+    var file_list = fs.readdirSync(config.images_path);
+    var random_pos = Math.floor(Math.random() * file_list.length;
+    return config.images_path+"/"+file_list[random_pos];
 }
 
 exports.getKittenImagePath = getKittenImagePath;
