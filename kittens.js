3fff0c3ce84203058cf04d883dbe7d4441d4525f
patillacode <patillacode@gmail.com>
2016-02-10 19:15:13 +0000

    Fixed syntax error.

--- kittens.js
+++ kittens.js
@@ -3,7 +3,7 @@ config = require('./config');
 
 function getKittenImagePath(){
     var file_list = fs.readdirSync(config.images_path);
-    var random_pos = Math.floor(Math.random() * file_list.length;
+    var random_pos = Math.floor(Math.random() * file_list.length);
     return config.images_path+"/"+file_list[random_pos];
 }
 
